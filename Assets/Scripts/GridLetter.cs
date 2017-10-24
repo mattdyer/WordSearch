da480ffb8e22f9d5f84b94e02bb3383bc9d60911
mattdyer <madmatt1220@gmail.com>
2017-10-23 22:06:54 -0600

    working event data for word highlighting

--- Assets/Scripts/GridLetter.cs
+++ Assets/Scripts/GridLetter.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using System.Collections.Generic;
 
 public class GridLetter : MonoBehaviour {
@@ -28,7 +29,21 @@ public class GridLetter : MonoBehaviour {
 		words.Add(word);
 	}
 
-	public void startWordDrag(){
+	/*public void startWordDrag(){
+		if(!isRandomLetter && !highlighting && isEndLetter){
+			highlighting = true;
+			for(int i = 0;i < words.Count;i++){
+				words[i].startDrag();
+			}
+			
+		}
+	}*/
+
+
+	public void startWordDrag(BaseEventData data){
+
+		Debug.Log(data);
+
 		if(!isRandomLetter && !highlighting && isEndLetter){
 			highlighting = true;
 			for(int i = 0;i < words.Count;i++){
@@ -38,7 +53,7 @@ public class GridLetter : MonoBehaviour {
 		}
 	}
 
-	public void endWordDrag(){
+	public void endWordDrag(BaseEventData data){
 		if(!isRandomLetter){
 			highlighting = false;
 			for(int i = 0;i < words.Count;i++){
@@ -47,7 +62,24 @@ public class GridLetter : MonoBehaviour {
 		}
 	}
 
-	public void enterLetter(){
+	/*public void enterLetter(){
+		
+		Event e = Event.current;
+
+		Debug.Log(e);
+
+		if(isEndLetter){
+			for(int i = 0;i < words.Count;i++){
+				if(words[i].highlighting && !highlighting){
+					words[i].highlightWord();
+				}
+			}
+		}
+	}*/
+
+	public void enterLetter(BaseEventData data){
+		Debug.Log(data);
+
 		if(isEndLetter){
 			for(int i = 0;i < words.Count;i++){
 				if(words[i].highlighting && !highlighting){
